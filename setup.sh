090adb088f68dbff056100372b31a418490af5fa
Vincent Donato <vincentdonato@pioneers.berkeley.edu>
2015-08-29 15:30:44 -0700

    Fix typo in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@
 # to as we figure out other things that need to be preconfigured for a
 # completely clean install.
 sudo usermod -a -G vboxusers $USER
-vagrant box add ubuntu/trusty64 https://atlas.hasicorp.com/ubuntu/boxes/trusty64
+vagrant box add ubuntu/trusty64 https://atlas.hashicorp.com/ubuntu/boxes/trusty64/versions/20150821.0.0/providers/virtualbox.box
