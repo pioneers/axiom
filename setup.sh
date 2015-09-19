5b93c9c34cbc67ac350c57cb2c4a5404f5763468
Vincent Donato <vincentdonato@pioneers.berkeley.edu>
2015-09-19 16:15:58 -0700

    Have setup script make a projects folder.

--- setup.sh
+++ setup.sh
@@ -4,5 +4,9 @@
 # virtualbox extensions have been installed. This script will likely be added
 # to as we figure out other things that need to be preconfigured for a
 # completely clean install.
+
 sudo usermod -a -G vboxusers $USER
 vagrant box add ubuntu/trusty64 https://atlas.hashicorp.com/ubuntu/boxes/trusty64/versions/20150821.0.0/providers/virtualbox.box
+if [ ! -d ./projects ]; then
+  mkdir projects
+fi
